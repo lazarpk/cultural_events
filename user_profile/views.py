56a1492eb1dc3d6d7d435559c71328d0088cd2ed
lazarpk <lazarospk95@gmail.com>
2021-09-09 20:11:26 +0200

    Dodata funkcija za update informacija o korisniku

--- user_profile/views.py
+++ user_profile/views.py
@@ -32,10 +32,42 @@ def details (request):
 
 
 def edit (request):
-    form = UpdateUserForm(instance=request.user)
-    form1 = UpdateProfileForm(instance=Profile.objects.get(user_id = request.user.id))
-    context = {
-        'form': form,
-        'form1': form1
-    }
-    return render (request, 'edit.html', context)
+    if (request.method == 'GET'):
+        form = UpdateUserForm(instance=request.user)
+        form1 = UpdateProfileForm(instance=Profile.objects.get(user_id = request.user.id))
+        context = {
+            'form': form,
+            'form1': form1
+        }
+        return render (request, 'edit.html', context)
+    elif (request.method == "POST"):
+        form = UpdateUserForm(request.POST, instance=request.user)
+        form1 = UpdateProfileForm(request.POST)
+        if (form.is_valid() and form1.is_valid()):
+            user = User.objects.get(id = request.user.id)
+            user.first_name = form.cleaned_data.get('first_name')
+            user.last_name = form.cleaned_data.get('last_name')
+            user.username = form.cleaned_data.get('username')
+            user.email = form.cleaned_data.get('email')
+            user.save()
+
+            profile = Profile.objects.get(user_id = request.user.id)
+            profile.address = form1.cleaned_data.get('address')
+            profile.number = form1.cleaned_data.get('number')
+            profile.city = form1.cleaned_data.get('city')
+            profile.contact_person = form1.cleaned_data.get('contact_person')
+            profile.phone = form1.cleaned_data.get('phone')
+            profile.description = form1.cleaned_data.get('description')
+            profile.work_area = form1.cleaned_data.get('work_area')
+            profile.web_site = form1.cleaned_data.get('web_site')
+            profile.save()
+
+            return render(request, 'details.html')
+        else:
+            form = UpdateUserForm(instance=request.user)
+            form1 = UpdateProfileForm(instance=Profile.objects.get(user_id = request.user.id))
+            context = {
+                'form': form,
+                'form1': form1
+            }
+            return render (request, 'edit.html', context)
