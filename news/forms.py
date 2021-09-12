bcb9f3765158e562130bf5ffabf5125ef9d9faca
lazarpk <lazarospk95@gmail.com>
2021-09-12 16:25:25 +0200

    Popravljanje forme za dodavanje vesti

--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,26 @@
 from django.forms import ModelForm
 from .models import Article
+from django.contrib.auth.forms import forms
 
 class ArticleForm(ModelForm):
 
     class Meta:
         model = Article
         fields = ['Title', 'Content', 'Category']
+        widgets = {
+            "title": forms.TextInput(
+                attrs={
+                    "class": "form-control"
+                }
+            ),
+            "content": forms.EmailInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            "category": forms.TextInput(
+                attrs={
+                    'class': 'form-control'
+                }
+            )
+        }
