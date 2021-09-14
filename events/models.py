dfdfbbbb81dc902ee0381cd822f3a33445d8dfa5
danicabogunovic <89413869+danicabogunovic@users.noreply.github.com>
2021-09-14 21:31:58 +0200

    Migrate to sql base

--- events/models.py
+++ events/models.py
@@ -7,11 +7,11 @@ class Events (models.Model):
     text = models.CharField (max_length = 200, default = '');
     type = models.CharField (max_length = 30, default = '');
     place = models.CharField (max_length = 30, default = '');
-    time = models.DateTimeField(default = '');
-    age = models.IntegerField(default = 0);
+    time = models.DateTimeField(default = timezone.now);
+    age = models.IntegerField(default = False);
     space_characteristics = models.CharField(max_length = 150, default = '');
     date_published = models.DateTimeField (default = timezone.now);
-    expiration_date = models.DateTimeField(default = '');
+    expiration_date = models.DateTimeField(default = timezone.now);
     author = models.CharField (max_length = 30, default = '');
     archive = models.CharField (max_length = 1, default = 'N');
     delete = models.CharField (max_length = 1, default = 'N');
